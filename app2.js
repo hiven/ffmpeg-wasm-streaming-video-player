5f837ba593119180fae2dd6dd05c79400df345a9
hiven <mail@hiven.com>
2024-04-22 14:10:15 +0100

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const { fromEvent } = rxjs;
-  const { map, switchMap, take, flatMap } = rxjs.operators;
+  const { Observable, fromEvent, combineLatest, zip } = rxjs;
+  const { map, flatMap, take, skip } = rxjs.operators;
 
   const fileInput = document.getElementById('fileInput');
-  const transcodeButton = document.getElementById('transcodeButton');
   const videoPlayer = document.getElementById('videoPlayer');
   const mediaSource = new MediaSource();
   videoPlayer.src = URL.createObjectURL(mediaSource);
@@ -11,57 +10,57 @@ document.addEventListener('DOMContentLoaded', () => {
   
   const mediaSourceOpen = fromEvent(mediaSource, 'sourceopen');
 
-  const bufferStream = filename => new Observable(async subscriber => {
-    const ffmpeg = FFmpeg.createFFmpeg({
-      corePath: 'thirdparty/ffmpeg-core.js',
-      log: false
+  const bufferStream = file =>
+    new Observable(async subscriber => {
+      const ffmpeg = FFmpeg.createFFmpeg({
+        corePath: 'thirdparty/ffmpeg-core.js',
+        log: false
+      });
+
+      const fileExists = file => ffmpeg.FS('readdir', '/').includes(file);
+      const readFile = file => ffmpeg.FS('readFile', file);
+
+      await ffmpeg.load();
+      const sourceBuffer = await file.arrayBuffer();
+      ffmpeg.FS('writeFile', 'input.mp4', new Uint8Array(sourceBuffer, 0, sourceBuffer.byteLength));
+
+      let index = 0;
+
+      ffmpeg.run(
+        '-i',
+        'input.mp4',
+        '-g',
+        '1',
+        '-segment_format_options',
+        'movflags=frag_keyframe+empty_moov+default_base_moof',
+        '-segment_time',
+        '5',
+        '-f',
+        'segment',
+        '%d.mp4'
+      ).then(() => {
+        while (fileExists(`${index}.mp4`)) {
+          subscriber.next(readFile(`${index}.mp4`));
+          index++;
+        }
+        subscriber.complete();
+      });
+
+      setInterval(() => {
+        if (fileExists(`${index + 1}.mp4`)) {
+          subscriber.next(readFile(`${index}.mp4`));
+          index++;
+        }
+      }, 200);
     });
 
-    const fileExists = file => ffmpeg.FS('readdir', '/').includes(file);
-    const readFile = file => ffmpeg.FS('readFile', file);
-
-    await ffmpeg.load();
-    const sourceBuffer = await fetch(filename).then(r => r.arrayBuffer());
-    ffmpeg.FS('writeFile', 'input.mp4', new Uint8Array(sourceBuffer, 0, sourceBuffer.byteLength));
-
-    let index = 0;
-
-    ffmpeg.run(
-      '-i',
-      'input.mp4',
-      '-g',
-      '1',
-      '-segment_format_options',
-      'movflags=frag_keyframe+empty_moov+default_base_moof',
-      '-segment_time',
-      '5',
-      '-f',
-      'segment',
-      '%d.mp4'
-    ).then(() => {
-      while (fileExists(`${index}.mp4`)) {
-        subscriber.next(readFile(`${index}.mp4`));
-        index++;
-      }
-      subscriber.complete();
-    });
-
-    setInterval(() => {
-      if (fileExists(`${index + 1}.mp4`)) {
-        subscriber.next(readFile(`${index}.mp4`));
-        index++;
-      }
-    }, 200);
-  });
-
-  const bufferStreamReady = fromEvent(transcodeButton, 'click').pipe(
-    switchMap(() => {
-      const file = fileInput.files[0];
+  const bufferStreamReady = fromEvent(fileInput, 'change').pipe(
+    map(event => event.target.files[0]),
+    flatMap(file => {
       if (!file) {
-        alert('Please select a file.');
         return [];
       }
-      return bufferStream(URL.createObjectURL(file));
+      return combineLatest(mediaSourceOpen, bufferStream(file)).pipe(map(([, a]) => a));
     })
   );
 
@@ -79,10 +78,10 @@ document.addEventListener('DOMContentLoaded', () => {
   );
 
   bufferStreamReady.pipe(
-    flatMap(buffer => fromEvent(sourceBuf, 'updateend').pipe(map(() => buffer)))
+    flatMap(buffer => fromEvent(buffer, 'updateend').pipe(map(() => buffer)))
   ).subscribe(buffer => {
     mediaSource.duration += 5;
-    sourceBuf.timestampOffset += 5;
-    sourceBuf.appendBuffer(buffer.buffer);
+    buffer.timestampOffset += 5;
+    buffer.appendBuffer(buffer.buffer);
   });
 });
